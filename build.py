735cce770c068b0c68a458187fcd1afe94a32f9c
Denis Jacquerye <denis.jacquerye@daltonmaag.com>
2015-11-06 14:36:50 +0000

    Add build.py

---
+++ build.py
@@ -0,0 +1,82 @@
+from __future__ import print_function, unicode_literals
+import sys
+import os
+from io import open
+from ufo2fdk import OTFCompiler
+from robofab.world import OpenFont
+
+try:
+    from plistlib import load
+except ImportError:
+    from plistlib import readPlist as load
+
+
+def groups_to_features(ufo_path):
+    """
+    Translate and copy UFO groups.list file to FEA features groups.
+    For example:
+    <dict>
+        <key>figDef</key>
+        <array>
+            <string>zero</string>
+            <string>one</string>
+            <string>two</string>
+            <string>three</string>
+            <string>four</string>
+            <string>five</string>
+            <string>six</string>
+            <string>seven</string>
+            <string>eight</string>
+            <string>nine</string>
+        </array>
+    </dict>
+    becomes
+    @figDef = [zero one two three four five six seven eight nine];
+    """
+    path = os.path.join(ufo_path, "groups.plist")
+    with open(path, "rb") as f:
+        groups = load(f)
+
+    output = []
+    for group, lst in groups.items():
+        if group.startswith("@"):
+            output.append("{} = [{}]".format(group, " ".join(lst)) + ";\n")
+        else:
+            output.append("@{} = [{}]".format(group, " ".join(lst)) + ";\n")
+
+    path = os.path.join(ufo_path, "features.fea")
+    with open(path, "r") as f:
+        features = f.readlines()
+
+    newFeatures = output + [""] + features
+
+    with open(path + ".bak", "w") as f:
+        f.writelines(features)
+    with open(path, "w") as f:
+        f.writelines(newFeatures)
+
+
+def build(family, styles):
+    for w in styles:
+        if w == "Regular":
+            ufo_path = "source/{}.ufo".format(family.replace(" ", "_"))
+        else:
+            ufo_path = "source/{}-{}.ufo".format(family.replace(" ", "_"), w)
+
+        groups_to_features(ufo_path)
+
+        font = OpenFont(ufo_path)
+        compiler = OTFCompiler(
+            savePartsNextToUFO=len(sys.argv) > 1 and sys.argv[1] == "debug")
+        reports = compiler.compile(font, family + "-" + w + ".otf",
+                                   checkOutlines=False, autohint=True,
+                                   releaseMode=True)
+
+        print(reports["autohint"])
+        print(reports["makeotf"])
+
+
+if __name__ == "__main__":
+    family = "Deck Slab"
+    styles = ["Regular"]
+    build(family, styles)
