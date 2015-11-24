b9f4c18d8d3175e66144af974610018575dc2fe0
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2015-11-24 10:40:32 +0000

    pass --no-info option to ttfautohint (the version will be recorded in the release notes)

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@
 
 python tools\build.py ^
 	--output-dir build ^
-	--autohint --tta-control-file source\ttfautohint.ctrl --tta-options "-D latn -f latn -l 11 -G 50" ^
+	--autohint --tta-control-file source\ttfautohint.ctrl --tta-options "-D latn -f latn -l 11 -G 50 -n" ^
 	--goadb source\GlyphOrderAndAliasDB.txt ^
 	source\ScopeOne_Rg.ufo %*
