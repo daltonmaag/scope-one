6c54fa37cf1417db9ffed06d61b2da6d3e06f2bb
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2016-02-11 13:36:35 +0000

    pass --detailed-info to ttfautohint to write the commandline args along with the version string

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@
 
 python tools\build.py ^
 	--output-dir build ^
-	--autohint --tta-control-file source\ttfautohint.ctrl --tta-options "-D latn -f latn -l 11 -G 50" ^
+	--autohint --tta-control-file source\ttfautohint.ctrl --tta-options "-I -D latn -f latn -l 11 -G 50" ^
 	--goadb source\GlyphOrderAndAliasDB.txt ^
 	source\ScopeOne_Rg.ufo %*
